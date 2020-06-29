e96cd33cca38189a973d7eeea4792495fcb6ffff
fsteff <fsteff@gmx.at>
2020-06-29 19:32:32 +0200

    init

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main w3-display-container w3-blue-gray">
+
+      <div className="w3-display-middle">
+        <header>
+          Stefan Fixl
+        </header>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Main;
